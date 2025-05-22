1154df63e413f71b2028008fe7b610b77d3f2cae
nadeem <nadeemshaji2240@gmail.com>
2025-05-22 16:40:38 +0530

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'hbd.fidhaa',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
+
